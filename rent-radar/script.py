18ae8133c461dc1f132fa5c68561eb4c6fdccbf5
John Allison <john123allison@gmail.com>
2025-01-24 16:55:51 -0500

    iniit branch commit

--- rent-radar/script.py
+++ rent-radar/script.py
@@ -14,16 +14,6 @@ logging.basicConfig(
     format='%(asctime)s %(levelname)s:%(message)s'
 )
 
-DATABASE_FILE = 'rental_listings.db'
-FLAGGED_CSV_FILE = 'flagged_listings.csv'
-
-# Reference date for anti-gouging check
-REFERENCE_DATE = datetime.date(2025, 1, 6)
-PRICE_INCREASE_THRESHOLD = 0.10  # 10% by law
-
-# Zillow rental search for Los Angeles County (has ~40k results)
-ZILLOW_BASE_URL = 'https://www.zillow.com/homes/for_rent/Los-Angeles-County-CA/'
-
 
 # -------------------------
 # Database Initialization
